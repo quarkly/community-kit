136be3a7312448be86279875aa68d205a3590cf2
Aleksandr Kudinov <aesarine@gmail.com>
2021-04-09 12:01:37 +0300

    feat: add Gallery component template

--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ export { default as Tooltip } from "./Tooltip";
 export { default as Timer } from "./Timer";
 export { default as BeforeAfterImage } from "./BeforeAfterImage";
 export { default as BgImageParallax } from "./BgImageParallax";
+export { default as Gallery } from "./Gallery";
