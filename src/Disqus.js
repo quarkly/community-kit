9d32b987a6d2ec352a19e758fb62238d98452292
Andrey Zhuravlev <crane.andrey@gmail.com>
2021-04-12 13:07:54 +0300

    fix: linter rules

--- src/Disqus.js
+++ src/Disqus.js
@@ -6,110 +6,103 @@ import atomize from '@quarkly/atomize';
 import ComponentNotice from './ComponentNotice';
 
 const overrides = {
-  'Disqus Content': {
-    kind: 'Disqus Content',
-    props: {
-      width: '100%',
-      height: 'auto',
+    'Disqus Content': {
+        kind: 'Disqus Content',
+        props: {
+            width: '100%',
+            height: 'auto',
+        },
     },
-  },
 };
 
 const Wrapper = atomize.div();
 const Content = atomize.div();
 
 const DisqusComponent = ({
-  shortnameProp,
-  languageProp,
-  identifierProp,
-  urlProp,
-  titleProp,
-  ...props
-}) => {	
-  const { override, rest } = useOverrides(props, overrides);
-  
-  const commentsParams = useMemo(() => ({
-    identifier: identifierProp,
-    url: urlProp,
-    title: titleProp,
-    language: languageProp,
-  }), [
+    shortnameProp,
+    languageProp,
     identifierProp,
-    urlProp, 
+    urlProp,
     titleProp,
-    languageProp
-  ]);
- 
-  return (
-    <Wrapper
-      width="100%"
-      {...rest}
-    >
-      <Content
-        {...override('Disqus Content')}
-        display={!identifierProp && 'none'}
-      >
-        <DiscussionEmbed
-          shortname={shortnameProp}
-          config={commentsParams}
-        />
-      </Content>
-    
-      {!identifierProp && (
-        <ComponentNotice
-          message="Добавьте ID обсуждения на панели Props"
-        />
-      )}
-    </Wrapper> 
-  )
+    ...props
+}) => {
+    const { override, rest } = useOverrides(props, overrides);
+
+    const commentsParams = useMemo(
+        () => ({
+            identifier: identifierProp,
+            url: urlProp,
+            title: titleProp,
+            language: languageProp,
+        }),
+        [identifierProp, urlProp, titleProp, languageProp]
+    );
+
+    return (
+        <Wrapper width="100%" {...rest}>
+            <Content
+                {...override('Disqus Content')}
+                display={!identifierProp && 'none'}
+            >
+                <DiscussionEmbed
+                    shortname={shortnameProp}
+                    config={commentsParams}
+                />
+            </Content>
+
+            {!identifierProp && (
+                <ComponentNotice message="Добавьте ID обсуждения на панели Props" />
+            )}
+        </Wrapper>
+    );
 };
 
 const propInfo = {
-  identifierProp: {
-    title: 'Идентификатор обсуждения',
-    control: 'input',
-    type: 'text',
-    category: 'Discussions',
-    weight: 1 
-  },
-  urlProp: {
-    title: 'URL-адрес обсуждения',
-    control: 'input',
-    type: 'text',
-    category: 'Discussions',
-    weight: 1 
-  },
-  titleProp: {
-    title: 'Заголовок обсуждения',
-    control: 'input',
-    type: 'text',
-    category: 'Discussions',
-    weight: 1 
-  },
-  shortnameProp: {
-    title: 'Имя вашей ленты',
-    control: 'input',
-    type: 'text',
-    category: 'General',
-    weight: .5
-  },
-  languageProp: {
-    title: 'Укажите язык',
-    control: 'input',
-    variants: ['en', 'de', 'fr', 'ru'],
-    type: 'text',
-    weight: .5
-  },
+    identifierProp: {
+        title: 'Идентификатор обсуждения',
+        control: 'input',
+        type: 'text',
+        category: 'Discussions',
+        weight: 1,
+    },
+    urlProp: {
+        title: 'URL-адрес обсуждения',
+        control: 'input',
+        type: 'text',
+        category: 'Discussions',
+        weight: 1,
+    },
+    titleProp: {
+        title: 'Заголовок обсуждения',
+        control: 'input',
+        type: 'text',
+        category: 'Discussions',
+        weight: 1,
+    },
+    shortnameProp: {
+        title: 'Имя вашей ленты',
+        control: 'input',
+        type: 'text',
+        category: 'General',
+        weight: 0.5,
+    },
+    languageProp: {
+        title: 'Укажите язык',
+        control: 'input',
+        variants: ['en', 'de', 'fr', 'ru'],
+        type: 'text',
+        weight: 0.5,
+    },
 };
 
 const defaultProps = {
-  languageProp: 'en',
-}; 
+    languageProp: 'en',
+};
 
-Object.assign(DisqusComponent, { 
-  propInfo,
-  defaultProps,
-  overrides,
+Object.assign(DisqusComponent, {
+    propInfo,
+    defaultProps,
+    overrides,
 });
 
 export default DisqusComponent;
--- src/DisqusComment.js
+++ src/DisqusComment.js
@@ -6,91 +6,86 @@ import atomize from '@quarkly/atomize';
 import ComponentNotice from './ComponentNotice';
 
 const overrides = {
-  'Disqus Content': {
-    kind: 'Disqus Content',
-    props: {
-      width: '100%',
-      height: 'auto',
+    'Disqus Content': {
+        kind: 'Disqus Content',
+        props: {
+            width: '100%',
+            height: 'auto',
+        },
     },
-  },
 };
 
 const Wrapper = atomize.div();
 const Content = atomize.div();
 
 const DisqusCommentComponent = ({
-  commentIDProp,
-  showParrent,
-  widthProp,
-  heightProp,
-  ...props
+    commentIDProp,
+    showParrent,
+    widthProp,
+    heightProp,
+    ...props
 }) => {
-  const { override, rest } = useOverrides(props, overrides);
-  
-  return (
-    <Wrapper
-      width="100%"
-      {...rest}
-    >
-      <Content
-        {...override('Disqus Content')}
-        display={!commentIDProp && 'none'}
-      >
-        <CommentEmbed
-          commentId={commentIDProp}
-          showParentComment={showParrent}
-          width={widthProp}
-          height={heightProp}
-        />
-      </Content>
-      
-      {!commentIDProp && (
-        <ComponentNotice
-          message="Добавьте ID комментария на панели Props"
-        />
-      )}
-    </Wrapper>
-  );
+    const { override, rest } = useOverrides(props, overrides);
+
+    return (
+        <Wrapper width="100%" {...rest}>
+            <Content
+                {...override('Disqus Content')}
+                display={!commentIDProp && 'none'}
+            >
+                <CommentEmbed
+                    commentId={commentIDProp}
+                    showParentComment={showParrent}
+                    width={widthProp}
+                    height={heightProp}
+                />
+            </Content>
+
+            {!commentIDProp && (
+                <ComponentNotice message="Добавьте ID комментария на панели Props" />
+            )}
+        </Wrapper>
+    );
 };
 
 const propInfo = {
-  commentIDProp: {
-    title: 'Идентификатор комментария',
-    control: 'input',
-    type: 'text',
-    weight: 1,
-  },
-  showParrent: {
-    title: 'Показать родительский комментарий',
-    control: 'checkbox',
-    type: 'text',
-    weight: 1,
-  },
-  widthProp: {
-    title: 'Ширина блока',
-    control: 'input',
-    type: 'text',
-    weight: 0.5,
-  },
-  heightProp: {
-    title: 'Высота блока',
-    control: 'input',
-    type: 'text',
-    weight: 0.5,
-  },
+    commentIDProp: {
+        title: 'Идентификатор комментария',
+        control: 'input',
+        type: 'text',
+        weight: 1,
+    },
+    showParrent: {
+        title: 'Показать родительский комментарий',
+        control: 'checkbox',
+        type: 'text',
+        weight: 1,
+    },
+    widthProp: {
+        title: 'Ширина блока',
+        control: 'input',
+        type: 'text',
+        weight: 0.5,
+    },
+    heightProp: {
+        title: 'Высота блока',
+        control: 'input',
+        type: 'text',
+        weight: 0.5,
+    },
 };
 
 const defaultProps = {
-  commentIDProp: '',
-  showParrent: false,
-  widthProp: '100%',
-  heightProp: '200px',
+    commentIDProp: '',
+    showParrent: false,
+    widthProp: '100%',
+    heightProp: '200px',
 };
 
 Object.assign(DisqusCommentComponent, {
-  propInfo,
-  defaultProps,
-  overrides,
+    propInfo,
+    defaultProps,
+    overrides,
 });
 
 export default DisqusCommentComponent;
