9d21d96c84e96f945a4f71e2bedd6fc023756d27
Andrey Zhuravlev <crane.andrey@gmail.com>
2021-04-13 15:33:58 +0300

    test: rendering exported components

--- src/__tests__/export.test.js
+++ src/__tests__/export.test.js
@@ -1,13 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
 import * as exported from '../index';
 
-describe('package export', () => {
-    it('exports components defined in quarklyInfo', () => {
-        const { quarklyInfo } = require('../../package.json');
+const { quarklyInfo } = require('../../package.json');
 
+describe('package export', () => {
+    test('package exports components defined in quarklyInfo', () => {
         // Компонентов в экспорте пакета, должно быть не меньше
         // чем описано в quarklyInfo
         expect(Object.keys(exported)).toEqual(
             expect.arrayContaining(Object.keys(quarklyInfo.components))
         );
+    });
+
+    test.each(
+        Object.keys(quarklyInfo.components).map((compName) => [compName])
+    )('%s component renders correctly', (componentName) => {
+        const Component = exported[componentName];
+        renderer.create(<Component />);
     });
 });
