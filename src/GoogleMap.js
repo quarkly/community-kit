3a1f41b03a087587d8ce923b3f98db86b6f62931
Andrey Zhuravlev <crane.andrey@gmail.com>
2021-04-21 17:54:04 +0300

    feat: GoogleMap component

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import atomize from "@quarkly/atomize";
+import atomize from '@quarkly/atomize';
 
-const GoogleMap = props => <div {...props}>Say hello GoogleMap</div>
+import ComponentNotice from './ComponentNotice';
 
-export default atomize(GoogleMap)({
-  name: "GoogleMap",
-  effects: {
-    hover: ":hover"
-  },
-  description: {
-    // paste here description for your component
-    en:
-      "GoogleMap — my awesome component",
-  },
-  propInfo: {
-    // paste here props description for your component
-    yourCustomProps: {
-      control: "input"
+const Map = ({ query = 'New York', apiKey, ...props }) => (
+    <div {...props}>
+        {apiKey ? (
+            <iframe
+                title={`community-kit-google-map-${query}`}
+                style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    top: 0,
+                    left: 0,
+                }}
+                frameBorder="0"
+                allowFullScreen
+                src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${query}`}
+            />
+        ) : (
+            <ComponentNotice message="Add API key in properties panel" />
+        )}
+    </div>
+);
+
+const GoogleMap = atomize(Map)(
+    {
+        name: 'GoogleMap',
+        propInfo: {
+            apiKey: {
+                control: 'input',
+                category: 'Main',
+            },
+            query: {
+                control: 'input',
+                category: 'Main',
+            },
+        },
+    },
+    {
+        width: '100%',
+        height: '450px',
+        position: 'relative',
     }
-  }
-});
+);
+
+export default GoogleMap;
