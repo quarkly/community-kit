6e2e9133bcdca06a1dd198098d22f851099972ce
Mobydack <devzetsubou@gmail.com>
2021-05-31 12:08:28 +0300

    fix(SimpleTabsItem): return old variant

--- src/SimpleTabsItem.js
+++ src/SimpleTabsItem.js
@@ -12,7 +12,7 @@ const SimpleTab = ({ index, ...props }) => {
     useEffect(() => {
         addTab({ tabId, index });
         return () => removeTab({ tabId, index });
-    }, [tabId, index, addTab, removeTab]);
+    }, [tabId, index]);
 
     const isHidden = currentTab !== tabId;
 
