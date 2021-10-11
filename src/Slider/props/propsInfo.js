53dd02408335bd4e9f1418bb29e2b6803d599f67
Maxim Slipenko <max.slipenko@gmail.com>
2021-10-11 12:53:00 +0300

    update localization

--- src/Slider/props/propsInfo.js
+++ src/Slider/props/propsInfo.js
@@ -2,20 +2,23 @@ export default {
     name: {
         title: {
             en: 'Input name',
+            ru: 'Имя input элемента',
         },
         control: 'input',
         type: 'string',
     },
     min: {
         title: {
-            en: 'Min',
+            en: 'Minimum value',
+            ru: 'Минимальное значение',
         },
         control: 'input',
         type: 'number',
     },
     max: {
         title: {
-            en: 'Max',
+            en: 'Maximum value',
+            ru: 'Максимальное значение',
         },
         control: 'input',
         type: 'number',
@@ -23,19 +26,26 @@ export default {
     stepSize: {
         title: {
             en: 'Step size',
+            ru: 'Размер шага',
+        },
+        description: {
+            en:
+                'Increment between successive values; amount by which the handle moves.',
         },
         control: 'input',
         type: 'number',
     },
     vertical: {
         title: {
-            en: 'Vertical',
+            en: 'Vertical orientation',
+            ru: 'Вертикальное отображение',
         },
         control: 'checkbox',
     },
     labelStepSize: {
         title: {
             en: 'Label step size',
+            ru: 'Размер шага надписей',
         },
         control: 'input',
         type: 'number',
@@ -43,6 +53,7 @@ export default {
     defaultValue: {
         title: {
             en: 'Default value',
+            ru: 'Значение по умолчанию',
         },
         control: 'input',
         type: 'number',
