4195a3d0449b5927cec0bb7bdb8fb3cc3775f4be
Maxim Slipenko <max.slipenko@gmail.com>
2021-11-03 15:12:17 +0300

    fix: remove unnecessary nesting in propInfo

--- src/TabsFullContent/TabsFullContent.js
+++ src/TabsFullContent/TabsFullContent.js
@@ -26,18 +26,15 @@ const TabsFullContent = ({ children, tabId, ...props }) => {
 
 const propInfo = {
     tabId: {
-        tabId: {
-            title: {
-                en: 'Tab ID',
-                ru: 'ID Вкладки',
-            },
-            description: {
-                en:
-                    'ID of the tab that is displayed when this button is clicked',
-                ru: 'ID вкладки, которая отображается при нажатии этой кнопки',
-            },
-            control: 'input',
+        title: {
+            en: 'Tab ID',
+            ru: 'ID Вкладки',
         },
+        description: {
+            en: 'ID of the tab that is displayed when this button is clicked',
+            ru: 'ID вкладки, которая отображается при нажатии этой кнопки',
+        },
+        control: 'input',
     },
 };
 
