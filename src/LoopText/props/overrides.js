f268b936fa103ac6449a5ad4786d872f0ec39689
Maxim Slipenko <max.slipenko@gmail.com>
2021-11-26 16:52:03 +0300

    change props order and refactor LoopText

---
+++ src/LoopText/props/overrides.js
@@ -0,0 +1,23 @@
+export default {
+    'Before Text': {
+        kind: 'Text',
+        props: {
+            display: 'inline-block',
+            children: 'The',
+        },
+    },
+    'After Text': {
+        kind: 'Text',
+        props: {
+            display: 'inline-block',
+            children: 'website builder!',
+        },
+    },
+    'Looped Text': {
+        kind: 'Text',
+        props: {
+            display: 'inline-block',
+            color: '--primary',
+        },
+    },
+};
