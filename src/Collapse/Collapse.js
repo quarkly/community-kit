61cf28f8f51c63aa6592277ef5f48027bebbddc6
Maxim Slipenko <max.slipenko@gmail.com>
2021-11-26 16:11:53 +0300

    move collapsedHeight outside of useEffect

--- src/Collapse/Collapse.js
+++ src/Collapse/Collapse.js
@@ -6,7 +6,6 @@ import { overrides, propInfo, defaultProps } from './props';
 import { pick } from '../utils';
 import reducer from './reducer';
 
-const raf = requestAnimationFrame;
 const pixelTransformer = getTransformer('pixel');
 
 const Collapse = ({
@@ -33,14 +32,14 @@ const Collapse = ({
         });
     }, []);
 
+    const collapsedHeight =
+        pixelTransformer(override('Wrapper :close').height) ?? 0;
+
     useEffect(() => {
         if (destination === 'none' || !ref.current) return;
 
         const transition = `height ${duration} ${animFunction}`;
         const expandedHeight = `${ref.current.scrollHeight}px`;
-        const collapsedHeight = pixelTransformer(
-            override('Wrapper :close').height ?? 0
-        );
 
         const [fromHeight, toHeight] =
             destination === 'open'
@@ -61,11 +60,11 @@ const Collapse = ({
             type: 'COLLAPSING',
         });
 
-        raf(() => {
+        requestAnimationFrame(() => {
             style.willChange = 'height';
             style.overflow = 'hidden';
             style.height = fromHeight;
-            raf(() => {
+            requestAnimationFrame(() => {
                 ref.current.style.transition = transition;
                 ref.current.style.height = toHeight;
             });
@@ -82,10 +81,7 @@ const Collapse = ({
 
         ref.current.removeEventListener('transitionend', handle);
         ref.current.addEventListener('transitionend', handle);
-
-        // override
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [animFunction, destination, duration]);
+    }, [animFunction, collapsedHeight, destination, duration]);
 
     return (
         <Box
