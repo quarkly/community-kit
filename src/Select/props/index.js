e7c10e67bc9944e3968f53e79c5dc785fe77c3b1
Maxim Slipenko <max.slipenko@gmail.com>
2021-12-07 16:34:46 +0300

    fix: remove import overrides

--- src/Select/props/index.js
+++ src/Select/props/index.js
@@ -1,4 +1,3 @@
-export { default as overrides } from './overrides';
 export { default as effects } from './effects';
 export { default as propInfo } from './propsInfo';
 export { default as defaultProps } from './propsDefault';
