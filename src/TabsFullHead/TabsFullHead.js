89cc7d09be91bdfcfd4c22800c61665da1814708
Maxim Slipenko <max.slipenko@gmail.com>
2021-12-26 12:45:40 +0300

    fix: remove extra overrides from Tabs

--- src/TabsFullHead/TabsFullHead.js
+++ src/TabsFullHead/TabsFullHead.js
@@ -1,13 +1,9 @@
 import React, { useMemo } from 'react';
 import styled from 'styled-components';
-import { Box } from '@quarkly/widgets';
-import { useOverrides } from '@quarkly/components';
-import { Placeholder } from '@quarkly/widgets/build/cjs/prod';
+import { Box, Placeholder } from '@quarkly/widgets';
 import { useTabs } from '../TabsFull';
 import ComponentNotice from '../ComponentNotice';
 import { isEmptyChildren } from '../utils';
-import { getHumanReadable } from '../TabsFull/utils';
-import overrides from './props/overrides';
 
 const NoScroll = styled(Box)`
     -ms-overflow-style: none;
@@ -17,32 +13,37 @@ const NoScroll = styled(Box)`
     }
 `;
 
-const TabsFullHead = (props) => {
-    const { override, children, rest } = useOverrides(props, overrides);
+const TabsFullHead = ({ children, ...props }) => {
     const context = useTabs();
     const { align, orientation } = context || {};
 
     const boxStyles = useMemo(() => {
         if (!align || !orientation) return {};
 
-        const {
-            orientation: orientationOverride,
-            align: alignOverride,
-        } = getHumanReadable({
-            orientation,
-            align,
-        });
+        const justifyContent = {
+            start: 'flex-start',
+            end: 'flex-end',
+            center: 'center',
+            'full width': 'full-width',
+        }[align];
 
-        return override(
-            'TabsFullHead',
-            `TabsFullHead ${alignOverride}`,
-            `TabsFullHead ${orientationOverride}`,
-            `TabsFullHead ${orientationOverride} ${alignOverride}`
-        );
-    }, [align, orientation, override]);
+        const flexDirection = {
+            vertical: 'column',
+            horizontal: 'row',
+        }[orientation];
+
+        return {
+            display: 'flex',
+            height: '100%',
+            width: '100%',
+            'justify-content': justifyContent,
+            'flex-direction': flexDirection,
+            padding: '0px 0px 5px 0px',
+        };
+    }, [align, orientation]);
 
     return (
-        <NoScroll overflow="auto" {...rest}>
+        <NoScroll overflow="auto" {...props}>
             {context ? (
                 <Box role="tablist" {...boxStyles}>
                     {children}
@@ -65,7 +66,6 @@ Object.assign(TabsFullHead, {
         ru:
             'Контейнер для TabsFullButton, которые являются ссылками на TabsFullContent. Этот компонент должен располагаться внутри TabsFull',
     },
-    overrides,
 });
 
 export default TabsFullHead;
