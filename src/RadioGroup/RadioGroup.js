218747e55c2136e1690d8ff580b997df525ca34b
Maxim Slipenko <max.slipenko@gmail.com>
2022-01-20 14:14:23 +0300

    fix: lint

--- src/RadioGroup/RadioGroup.js
+++ src/RadioGroup/RadioGroup.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { Box, Placeholder } from '@quarkly/widgets';
 import { propInfo, defaultProps } from './props';
 import RadioContext from './context/RadioContext';
