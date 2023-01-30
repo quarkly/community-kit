3c41b0abd17bce5ded91bd13dc3b9bd8ce53c087
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-01-30 22:37:39 +0300

    fix \n in production

--- src/CSVToTable/utils/parseCSV.js
+++ src/CSVToTable/utils/parseCSV.js
@@ -1,5 +1,8 @@
+import { getAPI } from '../../utils';
+
 export default (csvText, csvColumnDelimiter = ',') => {
-    const rows = csvText.split('\n');
+    const newline = getAPI().mode === 'development' ? '\n' : '\\n'
+    const rows = csvText.split(newline);
 
     return rows.map((row) => row.split(csvColumnDelimiter));
 };
