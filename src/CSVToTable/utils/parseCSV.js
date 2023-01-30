5775251de137798fcaac7da22bdb67e19e1780e4
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-01-30 22:42:19 +0300

    fix lint

--- src/CSVToTable/utils/parseCSV.js
+++ src/CSVToTable/utils/parseCSV.js
@@ -1,7 +1,7 @@
 import { getAPI } from '../../utils';
 
 export default (csvText, csvColumnDelimiter = ',') => {
-    const newline = getAPI().mode === 'development' ? '\n' : '\\n'
+    const newline = getAPI().mode === 'development' ? '\n' : '\\n';
     const rows = csvText.split(newline);
 
     return rows.map((row) => row.split(csvColumnDelimiter));
