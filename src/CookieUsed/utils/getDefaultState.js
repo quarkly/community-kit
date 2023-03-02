48a2cc0d11cf3011161159184d1eecc54ea82eac
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-03-02 14:50:57 +0300

    Fix CookieUsed component (#176)

--- src/CookieUsed/utils/getDefaultState.js
+++
@@ -1,7 +0,0 @@
-import storage from './storage';
-
-const getDefaultState = (isDev, show) => {
-    return isDev ? show : !storage.get();
-};
-
-export default getDefaultState;
--- src/CookieUsed/utils/index.js
+++
@@ -1,2 +0,0 @@
-export { default as storage } from './storage';
-export { default as getDefaultState } from './getDefaultState';
