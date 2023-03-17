446d642cf7814fc586c06c3897d3481f16694860
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-03-17 20:16:25 +0300

    remove propInfo

--- src/MenuWithGroups/MenuWithGroups.js
+++ src/MenuWithGroups/MenuWithGroups.js
@@ -8,7 +8,7 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 import { useRouteMatch } from 'react-router-dom';
 import { useMatch } from '@reach/router';
 import { getAPI } from '../utils';
-import { overrides, defaultProps } from './props';
+import { overrides, propInfo, defaultProps } from './props';
 
 const getParent = (pages, pageId) => {
     if (!pageId || !pages[pageId]) return null;
@@ -260,66 +260,6 @@ const MenuWithGroups = ({ depth, rootId, expand, tabState, ...props }) => {
             {...rest}
         />
     );
-};
-
-const propInfo = {
-    depth: {
-        title: {
-            en: 'Maximum nesting',
-            ru: 'Максимальная вложенность',
-        },
-        control: 'input',
-        type: 'number',
-        category: 'Main',
-        weight: 1,
-    },
-    rootId: {
-        title: {
-            en: 'Root page',
-            ru: 'Корневая страница',
-        },
-        control: 'href',
-        category: 'Main',
-        weight: 1,
-    },
-    tabState: {
-        title: {
-            en: 'Condition of groups by default',
-            ru: 'Состояние групп по умолчанию',
-        },
-        control: 'select',
-        variants: [
-            {
-                title: {
-                    en: 'Collapse all groups',
-                    ru: 'Свернуть все группы',
-                },
-                value: 'collapseAll',
-            },
-            {
-                title: {
-                    en: 'Expand before the active menu item',
-                    ru: 'Раскрыть перед активным пунктом',
-                },
-                value: 'expandActive',
-            },
-            {
-                title: {
-                    en: 'Expand all groups',
-                    ru: 'Раскрыть все группы',
-                },
-                value: 'expandAll',
-            },
-            {
-                title: {
-                    en: 'Keep all tabs open',
-                    ru: 'Держать все вкладки раскрытыми',
-                },
-                value: 'keepExpanded',
-            },
-        ],
-        weight: 1,
-    },
 };
 
 Object.assign(MenuWithGroups, {
