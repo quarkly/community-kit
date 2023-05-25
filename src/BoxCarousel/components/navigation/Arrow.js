80c439c2900cb869cc5b86ee1d8488bd2f295fb0
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-05-25 07:27:38 +0300

    Replace QButton with @quarkly/widgets Button

--- src/BoxCarousel/components/navigation/Arrow.js
+++ src/BoxCarousel/components/navigation/Arrow.js
@@ -1,15 +1,7 @@
-import atomize from '@quarkly/atomize';
-import { Icon, Button as QButton } from '@quarkly/widgets';
+import { Icon, Button } from '@quarkly/widgets';
 import React, { forwardRef, useCallback } from 'react';
 import { useBoxCarouselData } from '../../contexts/BoxCarouselData';
 import useSubscribe from '../../hooks/useSubscribe';
-import { pick } from '../../../utils';
-
-// TODO: Replace with @quarkly/widgets button
-const Button = atomize.button(
-    pick(QButton, 'name', 'effects', 'description', 'propInfo'),
-    QButton.defaultProps
-);
 
 const Arrow = forwardRef(({ direction, ...props }, ref) => {
     const { override, swiper } = useBoxCarouselData();
